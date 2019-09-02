ee85b2ff2d9f22097f7d4fcc338ee69e045e0591
zus <dimitrian.tkachenko@gmail.com>
2019-09-02 14:27:36 -0400

    Add Ubuntu WSL notes

--- init-vimrc.sh
+++ init-vimrc.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-ln -s ~/dot/vimrc ~/.vimrc
+# TODO: check prerequisites - git, cmake, python-dev
+VIMRC_FILE=~/.vimrc
+VIMRC_DOT_FILE=./vimrc
+if [ -f "$VIMRC_FILE" ]; then 
+    echo "$VIMRC_FILE exist"
+else
+    ln -s ~/dot/vimrc ~/.vimrc
+    echo "$VIMRC_FILE => $VIMRC_DOT_FILE"
+fi
+
+VUNDLE_DIR=~/.vim/bundle/Vundle.vim
+if [ -d "$VUNDLE_DIR" ]; then
+    echo "$VUNDLE_DIR exist"
+else
+    git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
+    vim +PluginInstall +qall
+    ~/.vim/bundle/YouCompleteMe/install.py --ts-completer
+fi
---
+++ ubuntu-wsl.md
@@ -0,0 +1,6 @@
+```
+sudo apt-get install nodejs
+sudo apt-get install cmake
+sudo apt-get install python2.7
+sudo apt-get install python-dev
+```
